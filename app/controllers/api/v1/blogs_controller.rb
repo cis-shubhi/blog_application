77faf0dcf0217cd78c586347df077a9c51d3f0e2
cis-shubhi <shubhi.b@cisinlabs.com>
2016-12-08 16:22:06 +0530

    encoded image

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,7 +1,8 @@
 class Api::V1::BlogsController < ApplicationController
   
   def index
-    @blogs = Blog.all
+    @blogs = Blog.all.map{ |b| { title: b.title, description: b.description, 
+                image: (b.image.present? ? "" : (Base64.encode64(b.image.file.file))), created_at: b.created_at } }
     render json: @blogs
   end
   def create
