9399199e69cbdda707e206e91796a1733d0e1054
cis-shubhi <shubhi.b@cisinlabs.com>
2016-12-13 15:21:06 +0530

    removed image from index

--- app/controllers/api/v1/blogs_controller.rb
+++ app/controllers/api/v1/blogs_controller.rb
@@ -1,9 +1,7 @@
 class Api::V1::BlogsController < ApplicationController
   
   def index
-    @blogs = Blog.all.map{ |b| { title: b.title, description: b.description, 
-                image: Base64.encode64(b.image.file.file), created_at: b.created_at } }
-    byebug
+    @blogs = Blog.all.map{ |b| { title: b.title, description: b.description, created_at: b.created_at } }
     render json: @blogs
   end
   def create
