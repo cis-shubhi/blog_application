e68e8f82387187ec0da0e019f6783c9fce631abc
cis-shubhi <shubhi.b@cisinlabs.com>
2016-12-15 15:28:27 +0530

    changed limit

--- db/migrate/20161215083816_change_column_image.rb
+++ db/migrate/20161215083816_change_column_image.rb
@@ -1,5 +1,5 @@
 class ChangeColumnImage < ActiveRecord::Migration[5.0]
   def change
-  	change_column  :blogs, :image, :text, :limit => 4294967295
+  	change_column  :blogs, :image, :text, :limit => 1073741824
   end
 end
